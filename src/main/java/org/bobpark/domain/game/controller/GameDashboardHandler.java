db88f93e1401a7b4a980d12443f511c9c5986305
Bob Park <hwpark@malgn.com>
2025-02-25 14:14:24 +0900

    feat: game dashboard connect handler 추가

---
+++ src/main/java/org/bobpark/domain/game/controller/GameDashboardHandler.java
@@ -0,0 +1,31 @@
+package org.bobpark.domain.game.controller;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.annotation.ConnectMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+import org.bobpark.domain.game.model.GameDashboardResponse;
+import org.bobpark.domain.game.service.GameDashboardService;
+
+@RequiredArgsConstructor
+@RestController
+public class GameDashboardHandler {
+
+    private final GameDashboardService gameDashboardService;
+
+    @ConnectMapping("connect")
+    public void onConnect(RSocketRequester requester) {
+        gameDashboardService.onConnect(requester);
+    }
+
+    @MessageMapping("gameDashboard.current")
+    public Mono<GameDashboardResponse> getCurrent() {
+        return gameDashboardService.getCurrent();
+    }
+
+}
